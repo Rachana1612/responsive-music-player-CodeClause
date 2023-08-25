8c716347061a16b045c32d62e09698a58fe3b70f
Rachana1612 <128033553+Rachana1612@users.noreply.github.com>
2023-08-25 15:39:15 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Take me away",
+    artist: "unknown",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Runaway",
+    artist: "Aurora",
+    img: "music-2",
+    src: "music-2"
+  },
+   {
+     name: "Don'T-Let-Break-Your-Heart",
+     artist: "louis-Tomlinson",
+     img: "music-3",
+     src: "music-3"
+   },
+   {
+     name: "Moments like this",
+     artist: "The afters",
+     img: "music-4",
+     src: "music-4"
+   },
+]
